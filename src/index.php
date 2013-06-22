9c5a2ca1c95f241d4d8b2463713d5351a8fcd9e4
Sam Stenvall <neggelandia@gmail.com>
2013-06-22 11:18:57 +0300

    use yii-configbuilder to build the configuration

--- src/index.php
+++ src/index.php
@@ -1,8 +1,7 @@
 <?php
 
-// change the following paths if necessary
+// change the following path if necessary
 $yii=dirname(__FILE__).'/../vendor/yiisoft/yii/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
@@ -12,5 +11,16 @@ defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 // include Composer's autoloader
 require_once(__DIR__.'/../vendor/autoload.php');
 
+// include yii-configbuilder and assemble the config
+$builder = __DIR__
+		.'/../vendor/crisu83/yii-configbuilder/helpers/ConfigBuilder.php';
+
 require_once($yii);
+require_once($builder);
+
+$config = ConfigBuilder::build(array(
+	__DIR__.'/protected/config/main.template.php',
+	__DIR__.'/protected/config/main.php'
+));
+
 Yii::createWebApplication($config)->run();
