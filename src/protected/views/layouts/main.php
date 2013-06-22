3da15404482d8e562c6600c3dc080191fd667bb4
Sam Stenvall <neggelandia@gmail.com>
2013-06-22 12:49:17 +0300

    import and register the Bootstrap LESS

--- src/protected/views/layouts/main.php
+++ src/protected/views/layouts/main.php
@@ -1,9 +1,17 @@
+<?php
+
+/* @var $cs CClientScript */
+$cs = Yii::app()->clientScript;
+$baseUrl = Yii::app()->baseUrl;
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
 		<title><?php echo Yii::app()->name ?></title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<?php $cs->registerCssFile($baseUrl.'/css/styles.css'); ?>
 	</head>
 	
 	<body>
