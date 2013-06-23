3a566d31be811ea901906809135b473f0273cbde
Sam Stenvall <neggelandia@gmail.com>
2013-06-24 00:16:06 +0300

    use actual page title instead of application name

--- src/protected/views/layouts/main.php
+++ src/protected/views/layouts/main.php
@@ -8,7 +8,7 @@ $baseUrl = Yii::app()->baseUrl;
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title><?php echo Yii::app()->name ?></title>
+		<title><?php echo $this->pageTitle; ?></title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<?php $cs->registerCssFile($baseUrl.'/css/styles.css'); ?>
