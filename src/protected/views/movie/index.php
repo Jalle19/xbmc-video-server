1d20498c8bd4b5f8ecabbee1b7110d2977b45e25
Sam Stenvall <neggelandia@gmail.com>
2013-06-24 00:16:27 +0300

    set proper page title

--- src/protected/views/movie/index.php
+++ src/protected/views/movie/index.php
@@ -1,8 +1,13 @@
-<h2>Movies</h2>
-
 <?php
 
 /* @var $dataProvider LibraryDataProvider */
+$this->pageTitle = 'Movies';
+
+?>
+
+<h2>Movies</h2>
+
+<?php
 
 $this->widget('zii.widgets.CListView', array(
 	'dataProvider'=>$dataProvider,
