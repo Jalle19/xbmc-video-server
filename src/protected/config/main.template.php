5ffd67f24a21ac1e9a0084d21ab47c5bc636656c
Sam Stenvall <neggelandia@gmail.com>
2013-06-23 15:50:00 +0300

    changed application name

--- src/protected/config/main.template.php
+++ src/protected/config/main.template.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'XBMC Video Server',
 
 	// preloading 'log' component
 	'preload'=>array('log', 'less'),
