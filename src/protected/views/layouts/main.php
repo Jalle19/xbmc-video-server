efadf031064bae9af8e3177e7fa530ff4f73add6
Sam Stenvall <neggelandia@gmail.com>
2013-06-23 15:51:22 +0300

    added initial navbar

--- src/protected/views/layouts/main.php
+++ src/protected/views/layouts/main.php
@@ -16,6 +16,32 @@ $baseUrl = Yii::app()->baseUrl;
 	
 	<body>
 		<div class="container">
+			
+			<div class="row">
+				<div class="span12">
+					<h1><?php echo Yii::app()->name; ?></h1>
+					<p class="lead">
+						Free your library
+					</p>
+				</div>
+			</div>
+			
+			<?php $this->widget('bootstrap.widgets.TbNavbar', array(
+				'brandLabel'=>false,
+				'display'=>TbHtml::NAVBAR_DISPLAY_NONE,
+				'collapse'=>true,
+				'items'=>array(
+					array(
+						'class'=>'bootstrap.widgets.TbNav',
+						'items'=>array(
+							array('label'=>'Movies', 'url'=>'#'),
+							array('label'=>'TV Shows', 'url'=>'#'),
+							array('label'=>'Settings', 'url'=>'#'),
+						),
+					),
+				),
+			)); ?>
+			
 			<?php echo $content; ?>
 		</div>
 	</body>
