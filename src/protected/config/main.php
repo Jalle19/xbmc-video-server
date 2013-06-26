88a6f89fac1968605f2de9ac561468b3fc8b2eb2
Sam Stenvall <neggelandia@gmail.com>
2013-06-26 23:47:21 +0300

    changed default controller to MovieController

--- src/protected/config/main.php
+++ src/protected/config/main.php
@@ -9,6 +9,8 @@ return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'XBMC Video Server',
 
+	'defaultController'=>'movie',
+
 	// preloading 'log' component
 	'preload'=>array('log', 'less'),
 	
