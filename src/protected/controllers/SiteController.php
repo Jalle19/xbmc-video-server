5f72ca49352e01adc9556858c9ef95eae87b2382
Sam Stenvall <neggelandia@gmail.com>
2013-06-26 21:36:01 +0300

    fix code warning

--- src/protected/controllers/SiteController.php
+++ src/protected/controllers/SiteController.php
@@ -8,6 +8,7 @@ class SiteController extends Controller
 	public function actionError()
 	{
 		if($error=Yii::app()->errorHandler->error)
+		if(($error=Yii::app()->errorHandler->error))
 		{
 			if(Yii::app()->request->isAjaxRequest)
 				echo $error['message'];
