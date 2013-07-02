28809ceb35a68b6257fb963bfd2a359d61fee74a
Sam Stenvall <neggelandia@gmail.com>
2013-07-02 23:27:09 +0300

    don't define YII_DEBUG by default

--- src/index.php
+++ src/index.php
@@ -5,9 +5,9 @@ $yii=dirname(__FILE__).'/../vendor/yiisoft/yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php'; 
 
 // remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
+//defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+//defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
 // include Composer's autoloader
 require_once(__DIR__.'/../vendor/autoload.php');
