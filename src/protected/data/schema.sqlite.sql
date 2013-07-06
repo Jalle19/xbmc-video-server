862600d929a546841e1f6a34167e2a607426c7f3
Sam Stenvall <neggelandia@gmail.com>
2013-07-06 10:49:57 +0300

    fixed the schema file, apparently SQLite doesn't like these types of comments

--- src/protected/data/schema.sqlite.sql
+++ src/protected/data/schema.sqlite.sql
@@ -1,4 +1,3 @@
-# TODO: Add migration
 DROP TABLE IF EXISTS "backends";
 CREATE TABLE "backend" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL, "name" VARCHAR NOT NULL, "hostname" VARCHAR NOT NULL, "port" INTEGER NOT NULL , "username" VARCHAR NOT NULL, "password" VARCHAR NOT NULL, "proxyLocation" VARCHAR, "default" INTEGER NOT NULL);
 
