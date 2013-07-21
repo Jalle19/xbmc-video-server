e349c7e672e1e43d3cfe6fe9ac559826cc2a30cc
Sam Stenvall <neggelandia@gmail.com>
2013-07-21 16:11:14 +0300

    added missing license and copyright

--- src/protected/components/FormHelper.php
+++ src/protected/components/FormHelper.php
@@ -4,6 +4,8 @@
  * Contains common helper functions needed for forms
  *
  * @author Sam Stenvall <neggelandia@gmail.com>
+ * @copyright Copyright &copy; Sam Stenvall 2013-
+ * @license https://www.gnu.org/licenses/gpl.html The GNU General Public License v3.0
  */
 class FormHelper
 {
