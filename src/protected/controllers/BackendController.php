Jalle19/xbmc-video-server

commit 340eacfb81cbe4482475b4cca7d12768748b2855

created a new base class for controllers that are only for administrators

src/protected/controllers/BackendController.php
@@ -7,7 +7,7 @@
  * @copyright Copyright &copy; Sam Stenvall 2013-
  * @license https://www.gnu.org/licenses/gpl.html The GNU General Public License v3.0
  */
-class BackendController extends Controller
+class BackendController extends AdminOnlyController
 {
 
 	/**
@@ -18,34 +18,6 @@ class BackendController extends Controller
 		parent::init();
 		
 		$this->defaultAction = 'admin';
-	}
-
-	/**
-	 * Returns the filters for this controller. In addition to parent filters 
-	 * we want access control.
-	 * @return array
-	 */
-	public function filters()
-	{
-		return array_merge(parent::filters(), array(
-			'accessControl',
-		));
-	}
-
-	/**
-	 * Returns the access control rules
-	 * @return array
-	 */
-	public function accessRules()
-	{
-		return array(
-			array('allow',
-				'expression'=>function() {
-					return Yii::app()->user->role == User::ROLE_ADMIN;
-				},
-			),
-			array('deny'),
-		);
 	}
 
 	/**

src/protected/components/AdminOnlyController.php
@@ -1,0 +1,41 @@
+<?php
+
+/**
+ * Base controller which only grants access to actions for administrators
+ *
+ * @author Sam Stenvall <neggelandia@gmail.com>
+ * @copyright Copyright &copy; Sam Stenvall 2013-
+ * @license https://www.gnu.org/licenses/gpl.html The GNU General Public License v3.0
+ */
+abstract class AdminOnlyController extends Controller
+{
+
+	/**
+	 * Returns the filters for this controller. In addition to parent filters 
+	 * we want access control.
+	 * @return array
+	 */
+	public function filters()
+	{
+		return array_merge(parent::filters(), array(
+			'accessControl',
+		));
+	}
+
+	/**
+	 * Returns the access control rules
+	 * @return array
+	 */
+	public function accessRules()
+	{
+		return array(
+			array('allow',
+				'expression'=>function() {
+					return Yii::app()->user->role == User::ROLE_ADMIN;
+				},
+			),
+			array('deny'),
+		);
+	}
+
+}

src/protected/controllers/UserController.php
@@ -7,36 +7,8 @@
  * @copyright Copyright &copy; Sam Stenvall 2013-
  * @license https://www.gnu.org/licenses/gpl.html The GNU General Public License v3.0
  */
-class UserController extends Controller
+class UserController extends AdminOnlyController
 {
-
-	/**
-	 * Returns the filters for this controller. In addition to parent filters 
-	 * we want access control.
-	 * @return array
-	 */
-	public function filters()
-	{
-		return array_merge(parent::filters(), array(
-			'accessControl',
-		));
-	}
-	
-	/**
-	 * Returns the access control rules
-	 * @return array
-	 */
-	public function accessRules()
-	{
-		return array(
-			array('allow',
-				'expression'=>function() {
-					return Yii::app()->user->role == User::ROLE_ADMIN;
-				},
-			),
-			array('deny'),
-		);
-	}
 
 	/**
 	 * Creates a new user

commit 01b0e13e7d7f8a2e1c5c2651d56adba49e5877a6

allow normal users to change backends (broke in 340eacfb81cbe4482475b4cca7d12768748b2855)

src/protected/controllers/BackendController.php
@@ -19,6 +19,18 @@ class BackendController extends AdminOnlyController
 		
 		$this->defaultAction = 'admin';
 	}
+	
+	/**
+	 * Override parent implementation to allow everyone to change backend
+	 * @return array the access control rules
+	 */
+	public function accessRules()
+	{
+		$rules = array(
+			array('allow', 'actions'=>array('change'))
+		);
+		return array_merge($rules, parent::accessRules());
+	}
 
 	/**
 	 * Override parent implementation so we don't get stuck in a redirect loop