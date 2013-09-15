a35d75b15cc1473884df56bc75b5bdea9ebbcb13
Sam Stenvall <neggelandia@gmail.com>
2013-09-15 13:45:19 +0300

    add commented out block for using MySQL in console.php too

--- src/protected/config/console.php
+++ src/protected/config/console.php
@@ -16,6 +16,17 @@ return array(
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/xbmc-video-server.db',
 		),
+		// To use MySQL instead of SQLite, uncomment the 'db' section below and 
+		// comment out the 'db' section above. You will then have to configure 
+		// the settings below to match your setup and run the file 
+		// schema.mysql.sql to setup the initial database.
+		/*'db'=>array(
+			'connectionString'=>'mysql:host=localhost;dbname=xbmc_video_server',
+			'emulatePrepare'=>true,
+			'username'=>'root',
+			'password'=>'',
+			'charset'=>'utf8',
+		),*/
 		'log'=>array(
 			'class'=>'CLogRouter',
 			'routes'=>array(
