50bcf25a4ba1d8e64b88c92a058fc637fb86f5b5
Sam Stenvall <neggelandia@gmail.com>
2013-09-15 13:46:55 +0300

    use monospace font for exceptions (fixes #50)

--- src/protected/views/log/view.php
+++ src/protected/views/log/view.php
@@ -7,19 +7,33 @@ $this->pageTitle = $pageTitle;
 ?>
 <h2><?php echo $pageTitle; ?></h2>
 
-<?php $this->widget('bootstrap.widgets.TbDetailView', array(
-	'type'=>TbHtml::DETAIL_TYPE_STRIPED,
-	'data'=>$model,
-	'attributes'=>array(
-		'level',
-		'category',
-		'logtime',
-		array(
-			'name'=>'message',
-			'type'=>'html',
-			'value'=>function($data) {
-				return nl2br($data->message);
-			}
+<div class="log-details">
+	<?php $this->widget('bootstrap.widgets.TbDetailView', array(
+		'type'=>TbHtml::DETAIL_TYPE_STRIPED,
+		'data'=>$model,
+		'attributes'=>array(
+			'level',
+			'category',
+			'logtime',
+			array(
+				'name'=>'message',
+				'type'=>'html',
+				'value'=>function($data) {
+					$message = nl2br($data->message);
+
+					// Format exceptions differently
+					if (strpos($data->category, 'exception') === 0)
+					{
+						$output = CHtml::openTag('div', array('class'=>'exception-message'));
+						$output .= $message;
+						$output .= CHtml::closeTag('div');
+
+						return $output;
+					}
+					else
+						return $message;
+				}
+			)
 		)
-	)
-));
+	)); ?>
+</div>
