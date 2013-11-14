8df6d4148ca26b5255d32e4d5e32268bec284fc0
Sam Stenvall <neggelandia@gmail.com>
2013-11-14 15:01:12 +0200

    fix null reference which happens when the "log" table doesn't exist (it won't until the user runs the application the first time)

--- src/protected/migrations/m131004_175217_add_ipaddress_column.php
+++ src/protected/migrations/m131004_175217_add_ipaddress_column.php
@@ -5,8 +5,14 @@ class m131004_175217_add_ipaddress_column extends CDbMigration
 
 	public function up()
 	{
-		// Check if column already exists
+		// Check that the table exists, if not it will be created automatically 
+		// later and we don't have to do anything
 		$table = Yii::app()->db->schema->getTable('log');
+
+		if ($table === null)
+			return true;
+
+		// Check if column already exists
 		$columns = $table->getColumnNames();
 
 		if (!in_array('source_address', $columns))
