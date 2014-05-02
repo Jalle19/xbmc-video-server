dd49e3e5c5272e6b9850b2f84de1b273dc064297
Sam Stenvall <neggelandia@gmail.com>
2014-05-02 14:52:00 +0300

    add yiic message configuration file

---
+++ src/protected/config/message.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * Configuration file for the "yiic message" command which generates translation 
+ * files
+ */
+return array(
+	'sourcePath'=>__DIR__.DIRECTORY_SEPARATOR.'..',
+	'messagePath'=>__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'messages',
+	'fileTypes'=>array('php'),
+	
+	// Change this to generate files for new languages
+	'languages'=>array('sv_se'),
+);
