4935f08697bd3a178caba1afdbf255d39c963edf
Sam Stenvall <neggelandia@gmail.com>
2014-05-02 13:48:43 +0300

    use different formatting for PHP errors too

--- src/protected/views/log/view.php
+++ src/protected/views/log/view.php
@@ -22,8 +22,9 @@ $this->pageTitle = $pageTitle;
 				'value'=>function($data) {
 					$message = nl2br($data->message);
 
-					// Format exceptions differently
-					if (strpos($data->category, 'exception') === 0)
+					// Format exceptions and core errors differently
+					if (strpos($data->category, 'exception') === 0 ||
+						strpos($data->category, 'php') === 0)
 					{
 						$output = CHtml::openTag('div', array('class'=>'exception-message'));
 						$output .= $message;
