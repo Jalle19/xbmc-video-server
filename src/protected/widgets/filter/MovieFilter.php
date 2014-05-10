Jalle19/xbmc-video-server

commit af2c22f916f1de69480581c878869b18247090ce

reduce code duplication in the result filter

src/protected/widgets/filter/MovieFilter.php
@@ -20,7 +20,7 @@ class MovieFilter extends VideoFilter
 
 			if ($typeaheadData === false)
 			{
-				$typeaheadData = CJavaScript::encode($this->getMovieNames());
+				$typeaheadData = CJavaScript::encode($this->getTypeaheadNames(VideoLibrary::getTVShows()));
 				Yii::app()->apiCallCache->set($cacheId, $typeaheadData);
 			}
 		}
@@ -46,20 +46,6 @@ class MovieFilter extends VideoFilter
 				array('empty'=>' ', 'style'=>'width: 120px;'));
 
 		echo $this->form->textFieldControlGroup($this->model, 'actor');
-	}
-	
-	/**
-	 * Returns an array containing all movie names
-	 * @return array the names
-	 */
-	private function getMovieNames()
-	{
-		$names = array();
-		
-		foreach (VideoLibrary::getMovies() as $movie)
-			$names[] = $movie->label;
-		
-		return $names;
 	}
 
 }

src/protected/widgets/filter/TVShowFilter.php
@@ -13,7 +13,7 @@ class TVShowFilter extends VideoFilter
 	protected function renderControls()
 	{
 		echo $this->form->typeaheadFieldControlGroup($this->model, 'name', 
-			CJavaScript::encode($this->getTVShowNames()));
+			CJavaScript::encode($this->getTypeaheadNames(VideoLibrary::getTVShows())));
 
 		echo $this->form->dropDownListControlGroup($this->model, 'genre', 
 				$this->model->getGenres(), array('empty'=>' '));
@@ -21,20 +21,6 @@ class TVShowFilter extends VideoFilter
 		echo $this->form->dropDownListControlGroup($this->model, 'watchedStatus', 
 				VideoFilterForm::getWatchedStatuses(), 
 				array('empty'=>' ', 'style'=>'width: 120px;'));
-	}
-	
-	/**
-	 * Returns an array containing the names of all TV shows
-	 * @return array the names
-	 */
-	private function getTVShowNames()
-	{
-		$names = array();
-
-		foreach (VideoLibrary::getTVShows() as $movie)
-			$names[] = $movie->label;
-
-		return $names;
 	}
 
 }

src/protected/widgets/filter/VideoFilter.php
@@ -82,5 +82,19 @@ abstract class VideoFilter extends CWidget
 			'disabled'=>$this->model->isEmpty(),
 			'url'=>$controller->createUrl($controller->route)));
 	}
+	
+	/**
+	 * Returns an array of names for a typeahead field
+	 * @param stdClass[] $mediaSource the source data
+	 */
+	protected function getTypeaheadNames($mediaSource)
+	{
+		$names = array();
+
+		foreach ($mediaSource as $media)
+			$names[] = $media->label;
+
+		return $names;
+	}
 
 }

commit b87d7dc0cbc1784cf78bc5f1c87aae469227bc68

fix regression caused by af2c22f (not noticable when caching is enabled)

src/protected/widgets/filter/MovieFilter.php
@@ -20,12 +20,12 @@ class MovieFilter extends VideoFilter
 
 			if ($typeaheadData === false)
 			{
-				$typeaheadData = CJavaScript::encode($this->getTypeaheadNames(VideoLibrary::getTVShows()));
+				$typeaheadData = CJavaScript::encode($this->getTypeaheadNames(VideoLibrary::getMovies()));
 				Yii::app()->apiCallCache->set($cacheId, $typeaheadData);
 			}
 		}
 		else
-			$typeaheadData = CJavaScript::encode($this->getMovieNames());
+			$typeaheadData = CJavaScript::encode($this->getTypeaheadNames(VideoLibrary::getMovies()));
 
 		echo $this->form->typeaheadFieldControlGroup($this->model, 'name', $typeaheadData);
 		echo $this->form->dropDownListControlGroup($this->model, 'genre', 