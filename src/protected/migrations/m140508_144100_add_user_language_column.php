cc7be9cd1ac424caec635443a27c859b7473be8f
Sam Stenvall <neggelandia@gmail.com>
2014-05-14 13:04:49 +0300

    add a column check to the latest migration

--- src/protected/migrations/m140508_144100_add_user_language_column.php
+++ src/protected/migrations/m140508_144100_add_user_language_column.php
@@ -5,7 +5,16 @@ class m140508_144100_add_user_language_column extends CDbMigration
 
 	public function up()
 	{
-		$this->addColumn('user', 'language', "string NULL");
+		$table = Yii::app()->db->schema->getTable('user');
+
+		if ($table === null)
+			return false;
+
+		// Check if column already exists
+		$columns = $table->getColumnNames();
+
+		if (!in_array('language', $columns))
+			$this->addColumn('user', 'language', 'string NULL');
 	}
 
 	public function down()
