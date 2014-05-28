0d0444792d3f7606b5e8de11a5581869803f6a3b
Sam Stenvall <neggelandia@gmail.com>
2014-05-28 17:49:46 +0300

    remove a pointless wrapper div, this way we can also reuse the styles that were previously defined inside it

--- src/protected/views/movie/details.php
+++ src/protected/views/movie/details.php
@@ -7,122 +7,120 @@
 $this->pageTitle = $details->getDisplayName().' - '.Yii::t('Movies', 'Movies');
 
 ?>
-<div class="item-details">
-	<div class="row">
-		<div class="span3">
-			<?php 
-			
-			echo CHtml::image(new ThumbnailVideo($details->thumbnail, 
-					Thumbnail::SIZE_LARGE), '', array(
-				'class'=>'item-thumbnail hidden-phone',
-			));
-			
-			// The check is also done in RetrieveMediaWidget but we don't want 
-			// to show this piece of text at all if the user can't do what it 
-			// says
-			if (Yii::app()->user->role !== User::ROLE_SPECTATOR)
-			{
-				?>
-				<div class="item-links">
-					<h3><?php echo Yii::t('Movies', 'Watch / Download'); ?></h3>
+<div class="row">
+	<div class="span3">
+		<?php 
 
-					<p>
-						<?php echo Yii::t('Movies', 'Click the Watch button to start streaming the video (open 
-						the file in your favorite media player), or download the 
-						individual files for later viewing using the links below it.'); ?>
-					</p>
+		echo CHtml::image(new ThumbnailVideo($details->thumbnail, 
+				Thumbnail::SIZE_LARGE), '', array(
+			'class'=>'item-thumbnail hidden-phone',
+		));
 
-					<?php $this->widget('RetrieveMovieWidget', array(
-						'links'=>$movieLinks,
-						'details'=>$details,
-					)); ?>
-				</div>
-				<?php
-			}
-			
+		// The check is also done in RetrieveMediaWidget but we don't want 
+		// to show this piece of text at all if the user can't do what it 
+		// says
+		if (Yii::app()->user->role !== User::ROLE_SPECTATOR)
+		{
 			?>
-			
-		</div>
-		
-		<div class="span9 item-description">
-			<div class="item-top row-fluid">
-				<div class="item-title span6">
-					<h2>
-						<a href="<?php echo $details->getIMDbUrl(); ?>" target="_blank">
-							<?php echo $details->label; ?>
-						</a>
-					</h2>
+			<div class="item-links">
+				<h3><?php echo Yii::t('Movies', 'Watch / Download'); ?></h3>
 
-					<p>(<?php echo $details->year; ?>)</p>
-
-					<p class="tagline">
-						<?php echo $details->tagline; ?>
-					</p>
-
-				</div>
-				
-				<div class="span6 hidden-phone">
-					<?php $this->widget('MediaFlags', array(
-						'streamDetails'=>$details->streamdetails,
-						'file'=>$details->file
-					)); ?>
-				</div>
-			</div>
-			
-			<div class="item-info clearfix">
-				
-				<?php
-				
-				if ($details->hasRating())
-					$this->renderPartial('/videoLibrary/_rating', array(
-						'item'=>$details));
-				
-				?>
-
-				<div class="pull-left">
-
-					<div class="item-metadata clearfix">
-
-						<p><?php echo $details->getGenreString(); ?></p>
-						<?php
-
-						// Runtime and MPAA rating are not always available
-						$runtime = $details->getRuntimeString();
-						
-						if ($runtime)
-							echo CHtml::tag('p', array(), $runtime);
-						
-						$this->widget('MPAARating', array(
-							'rating'=>$details->mpaa));
-						
-						?>
-					</div>
-
-				</div>
-			</div>
-			
-			<h3><?php echo Yii::t('Media', 'Plot'); ?></h3>
-			
-			<div class="item-plot">
 				<p>
-					<?php echo $details->getPlot(); ?>
+					<?php echo Yii::t('Movies', 'Click the Watch button to start streaming the video (open 
+					the file in your favorite media player), or download the 
+					individual files for later viewing using the links below it.'); ?>
 				</p>
-			</div>
-			
-			<h3><?php echo Yii::t('Media', 'Cast'); ?></h3>
-			
-			<?php echo FormHelper::helpBlock(Yii::t('Movies', "Click an image to see other movies with that person, or click the name to go to the person's IMDb page")); ?>
-			
-			<div class="row-fluid">
-				<?php $this->widget('zii.widgets.CListView', array(
-					'dataProvider'=>$actorDataProvider,
-					'itemView'=>'_actorGridItem',
-					'itemsTagName'=>'ul',
-					'itemsCssClass'=>'thumbnails actor-grid',
-					'enablePagination'=>false,
-					'template'=>'{items}'
+
+				<?php $this->widget('RetrieveMovieWidget', array(
+					'links'=>$movieLinks,
+					'details'=>$details,
 				)); ?>
 			</div>
+			<?php
+		}
+
+		?>
+
+	</div>
+
+	<div class="span9 item-description">
+		<div class="item-top row-fluid">
+			<div class="item-title span6">
+				<h2>
+					<a href="<?php echo $details->getIMDbUrl(); ?>" target="_blank">
+						<?php echo $details->label; ?>
+					</a>
+				</h2>
+
+				<p>(<?php echo $details->year; ?>)</p>
+
+				<p class="tagline">
+					<?php echo $details->tagline; ?>
+				</p>
+
+			</div>
+
+			<div class="span6 hidden-phone">
+				<?php $this->widget('MediaFlags', array(
+					'streamDetails'=>$details->streamdetails,
+					'file'=>$details->file
+				)); ?>
+			</div>
+		</div>
+
+		<div class="item-info clearfix">
+
+			<?php
+
+			if ($details->hasRating())
+				$this->renderPartial('/videoLibrary/_rating', array(
+					'item'=>$details));
+
+			?>
+
+			<div class="pull-left">
+
+				<div class="item-metadata clearfix">
+
+					<p><?php echo $details->getGenreString(); ?></p>
+					<?php
+
+					// Runtime and MPAA rating are not always available
+					$runtime = $details->getRuntimeString();
+
+					if ($runtime)
+						echo CHtml::tag('p', array(), $runtime);
+
+					$this->widget('MPAARating', array(
+						'rating'=>$details->mpaa));
+
+					?>
+				</div>
+
+			</div>
+		</div>
+
+		<h3><?php echo Yii::t('Media', 'Plot'); ?></h3>
+
+		<div class="item-plot">
+			<p>
+				<?php echo $details->getPlot(); ?>
+			</p>
+		</div>
+
+		<h3><?php echo Yii::t('Media', 'Cast'); ?></h3>
+
+		<?php echo FormHelper::helpBlock(Yii::t('Movies', "Click an image to see other movies with that person, or click the name to go to the person's IMDb page")); ?>
+
+		<div class="row-fluid">
+			<?php $this->widget('zii.widgets.CListView', array(
+				'dataProvider'=>$actorDataProvider,
+				'itemView'=>'_actorGridItem',
+				'itemsTagName'=>'ul',
+				'itemsCssClass'=>'thumbnails actor-grid',
+				'enablePagination'=>false,
+				'template'=>'{items}'
+			)); ?>
 		</div>
 	</div>
 </div>
--- src/protected/views/tvShow/recentlyAdded.php
+++ src/protected/views/tvShow/recentlyAdded.php
@@ -7,10 +7,11 @@ $this->pageTitle = $title = Yii::t('TVShows', 'Recently added episodes');
 ?>
 <h2><?php echo $title; ?></h2>
 
-<?php echo FormHelper::helpBlock(Yii::t('TVShows', 'You can click the show name to jump directly to the show details page')); ?>
+<?php 
 
-<div class="item-details">
-	<?php $this->widget('RecentlyAddedEpisodeList', array(
-		'dataProvider'=>$dataProvider,
-	)); ?>
-</div>
+echo FormHelper::helpBlock(Yii::t('TVShows', 'You can click the show name to jump directly to the show details page'));
+
+$this->widget('RecentlyAddedEpisodeList', array(
+	'dataProvider'=>$dataProvider,
+)); 
+	
