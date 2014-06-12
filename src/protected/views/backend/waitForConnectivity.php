68f8ebf25d76b016a0ac32f1502e64b48c491caa
Sam Stenvall <neggelandia@gmail.com>
2014-06-12 10:42:23 +0300

    add a message to the "waiting for connectivity" page that tells the user to manually power on the backend if it is completely powered off (fixes #145)

--- src/protected/views/backend/waitForConnectivity.php
+++ src/protected/views/backend/waitForConnectivity.php
@@ -8,6 +8,7 @@ $this->pageTitle = $title = Yii::t('Backend', 'Waiting for connection');
 	<img class="large-loader" src="<?php echo Yii::app()->baseUrl.'/images/loader-large.gif'; ?>" />
 
 	<p>
-		<?php echo Yii::t('Backend', 'Wake on LAN packet sent, this page will be automatically refreshed once the backend is reachable'); ?>
+		<p><?php echo Yii::t('Backend', 'Wake on LAN packet sent, this page will be automatically refreshed once the backend is reachable'); ?></p>
+		<p><?php echo Yii::t('Backend', "If the backend has been powered off completely you'll have to turn it on manually."); ?></p>
 	</p>
 </div>
