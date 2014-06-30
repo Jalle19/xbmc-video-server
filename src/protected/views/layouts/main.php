822db827115d50bd5586a4a2bb22955810c02750
Sam Stenvall <neggelandia@gmail.com>
2014-06-30 14:09:09 +0300

    change the initial viewport scale, this way the grid is shown as an actual grid when a phone is in portrait mode (previously it was always shown in list mode). Fixes #163
    fix changing display mode for seasons

--- src/protected/views/layouts/main.php
+++ src/protected/views/layouts/main.php
@@ -10,7 +10,7 @@ $baseUrl = Yii::app()->baseUrl;
 	<head>
 		<title><?php echo strip_tags($this->pageTitle); ?></title>
 		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta name="viewport" content="width=device-width, initial-scale=0.65" />
 		<?php 
 		
 		// Make sure jQuery is registered first
