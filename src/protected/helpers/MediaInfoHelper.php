a51fb9bd81ddb0d8acdf57d82a3ea7f3ec20fc42
Sam Stenvall <neggelandia@gmail.com>
2014-09-20 13:39:03 +0300

    add a needsTranscoding helper method to MediaInfoHelper

--- src/protected/helpers/MediaInfoHelper.php
+++ src/protected/helpers/MediaInfoHelper.php
@@ -11,6 +11,31 @@
 class MediaInfoHelper
 {
 
+	const VIDEO_CODEC_H264 = 'avc1';
+	const AUDIO_CODEC_AAC = 'aac';
+	const CONTAINER_MP4 = 'mp4';
+
+	/**
+	 * Checks whether the specified will need transcoding in order to be 
+	 * playable in browsers. For now it assumes that all H.264/AAC/MP4 files 
+	 * are playable directly, which is not strictly true.
+	 * @param File $file
+	 * @return boolean whether the file needs transcoding
+	 */
+	public static function needsTranscoding($file)
+	{
+		if (!self::hasMediaInfo($file->streamdetails))
+			return true;
+
+		$videoCodec = $file->streamdetails->video[0]->codec;
+		$audioCodec = $file->streamdetails->audio[0]->codec;
+		$fileInfo = new SplFileInfo($file->file);
+
+		return !($fileInfo->getExtension() === self::CONTAINER_MP4 &&
+				$videoCodec === self::VIDEO_CODEC_H264 &&
+				$audioCodec === self::AUDIO_CODEC_AAC);
+	}
+
 	/**
 	 * Checks whether the stream details contain the necessary media info
 	 * @return boolean
