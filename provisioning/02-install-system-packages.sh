9cadfbe432432cdd0fc574fc2e74dd9d3fdffa11
Sam Stenvall <neggelandia@gmail.com>
2015-08-08 21:25:29 +0300

    split system configuration provisioning step to a separate file

--- provisioning/02-install-system-packages.sh
+++ provisioning/02-install-system-packages.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# disable IPv6 (doesn't work if the host runs Windows and is on wireless)
-echo "net.ipv6.conf.all.disable_ipv6 = 1" > /etc/sysctl.d/60-disable-ipv6.conf
-service procps start > /dev/null 2>&1
-
 # install dependencies
 apt-get -qq update
 apt-get -y upgrade > /dev/null 2>&1
