925b7b956a5578e033a8f712087b325a08b7e3c8
Sam Stenvall <neggelandia@gmail.com>
2015-08-08 21:25:51 +0300

    enable a swap partition (Composer seems to need it)

--- provisioning/01-configure-system.sh
+++ provisioning/01-configure-system.sh
@@ -3,3 +3,8 @@
 # disable IPv6 (doesn't work if the host runs Windows and is on wireless)
 echo "net.ipv6.conf.all.disable_ipv6 = 1" > /etc/sysctl.d/60-disable-ipv6.conf
 service procps start > /dev/null 2>&1
+
+# enable swap
+dd if=/dev/zero of=/tmp/swap bs=1M count=512 > /dev/null 2>&1
+mkswap /tmp/swap > /dev/null 2>&1
+swapon /tmp/swap > /dev/null 2>&1
