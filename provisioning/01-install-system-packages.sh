8a6d259854f19887bcbede6fb60f814e0c82e571
Sam Stenvall <neggelandia@gmail.com>
2015-08-08 21:10:05 +0300

    fix ability to run "grunt" after vagrant up (fixes #239)

--- provisioning/01-install-system-packages.sh
+++ provisioning/01-install-system-packages.sh
@@ -7,4 +7,4 @@ service procps start > /dev/null 2>&1
 # install dependencies
 apt-get -qq update
 apt-get -y upgrade > /dev/null 2>&1
-apt-get -y install libapache2-mod-php5 php5-gd php5-cli php5-sqlite php5-json curl nodejs npm > /dev/null 2>&1
+apt-get -y install libapache2-mod-php5 php5-gd php5-cli php5-sqlite php5-json curl nodejs nodejs-legacy npm > /dev/null 2>&1
--- provisioning/02-install-npm-dependencies.sh
+++ provisioning/02-install-npm-dependencies.sh
@@ -2,5 +2,8 @@
 
 cd /vagrant
 
+# grunt-cli needs to be installed globally
+npm install -g grunt-cli
+
 # --no-bin-links is required on Windows
 npm install --no-bin-links > /dev/null 2>&1
