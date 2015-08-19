281008b135ac958ccdb61763e4b9b9099753f6fb
Sam Stenvall <neggelandia@gmail.com>
2015-08-20 00:03:45 +0300

    silence complaints from swapoff (closes #246)

--- provisioning/01-configure-system.sh
+++ provisioning/01-configure-system.sh
@@ -5,7 +5,7 @@ echo "net.ipv6.conf.all.disable_ipv6 = 1" > /etc/sysctl.d/60-disable-ipv6.conf
 service procps start > /dev/null 2>&1
 
 # enable swap (disable first to prevent errors during re-provisioning)
-swapoff /tmp/swap
+swapoff /tmp/swap > /dev/null 2>&1
 dd if=/dev/zero of=/tmp/swap bs=1M count=512 > /dev/null 2>&1
 mkswap /tmp/swap > /dev/null 2>&1
 swapon /tmp/swap > /dev/null 2>&1
