b2581622cb4fb2c5f4a7a3b663cab74be6f1c50c
Sam Stenvall <sam.stenvall@nordsoftware.com>
2015-08-29 12:30:16 +0300

    enable SSL site in the Vagrant environment (why not)

--- provisioning/04-configure-apache.sh
+++ provisioning/04-configure-apache.sh
@@ -3,10 +3,12 @@
 # disable default site
 a2dissite 000-default > /dev/null 2>&1
 
-# install site
+# install sites
 cp /vagrant/provisioning/etc/apache2/sites-available/xbmc-video-server.conf /etc/apache2/sites-available
+cp /vagrant/provisioning/etc/apache2/sites-available/xbmc-video-server-ssl.conf /etc/apache2/sites-available
 a2ensite xbmc-video-server > /dev/null 2>&1
+a2ensite xbmc-video-server-ssl > /dev/null 2>&1
 
 # enable modules and restart
-a2enmod rewrite expires rewrite > /dev/null 2>&1
+a2enmod rewrite expires rewrite ssl > /dev/null 2>&1
 service apache2 restart > /dev/null 2>&1
