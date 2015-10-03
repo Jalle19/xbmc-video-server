afba5b185ae61da0d581575b4e93b3755dfae6d5
Sam Stenvall <sam.stenvall@nordsoftware.com>
2015-10-03 17:41:00 +0300

    remove schema caching, it seems it's simply impossible to flush the cache properly when doing database migrations

--- src/protected/config/_db.php
+++ src/protected/config/_db.php
@@ -3,7 +3,7 @@
 // return the configuration for the 'db' application component
 return array(
 	'connectionString' => 'sqlite:'.__DIR__.'/../data/xbmc-video-server.db',
-	'schemaCachingDuration'=>2592000, // 30 days
+	'schemaCachingDuration'=>0, // 30 days
 );
 
 // To use MySQL instead of SQLite, use something like this instead. You will 
