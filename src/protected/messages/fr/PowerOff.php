4098ebb3160941fc6f28dbef4731000347ae8248
Sam Stenvall <sam.stenvall@nordsoftware.com>
2015-10-22 10:46:44 +0300

    fixed syntax error in one of the French translation files

--- src/protected/messages/fr/PowerOff.php
+++ src/protected/messages/fr/PowerOff.php
@@ -17,7 +17,7 @@
  * NOTE, this file must be saved in UTF-8 encoding.
  */
 return array (
-  'Hibernate' => 'Mettre en veille prolongée'
+  'Hibernate' => 'Mettre en veille prolongée',
   'Power off' => 'Éteindre',
   'Reboot' => 'Redémarrer',
   'Shutdown' => 'Éteindre',
