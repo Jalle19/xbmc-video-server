b12068c24aab32c15879ce6e7820c8e1fc3aa816
Sam Stenvall <sam.stenvall@nordsoftware.com>
2019-07-11 21:20:41 +0300

    Load the Composer autoloader before Yii configuration files
    Same thing is done in yiic.php already

--- src/index.php
+++ src/index.php
@@ -1,5 +1,8 @@
 <?php
 
+// include Composer's autoloader
+require_once __DIR__ . '/../vendor/autoload.php';
+
 // change the following path if necessary
 $yii    = __DIR__ . '/../vendor/yiisoft/yii/framework/yii.php';
 $config = __DIR__ . '/protected/config/main.php';
@@ -8,9 +11,6 @@ $config = __DIR__ . '/protected/config/main.php';
 //defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 //defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-
-// include Composer's autoloader
-require_once __DIR__ . '/../vendor/autoload.php';
 
 // increase memory limit and execution time
 ini_set('memory_limit', '1024M');
