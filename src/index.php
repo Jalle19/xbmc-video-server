b565b760bde0df0c0cffcf882eeb9ed663808554
Sam Stenvall <sam.stenvall@nordsoftware.com>
2019-07-11 19:49:25 +0300

    Use __DIR__ instead of dirname(__FILE__)
    __DIR__ has been available since forever, and we depend on PHP 5.4 or newer so it's fine

--- src/index.php
+++ src/index.php
@@ -1,8 +1,8 @@
 <?php
 
 // change the following path if necessary
-$yii=dirname(__FILE__).'/../vendor/yiisoft/yii/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php'; 
+$yii    = __DIR__ . '/../vendor/yiisoft/yii/framework/yii.php';
+$config = __DIR__ . '/protected/config/main.php';
 
 // remove the following lines when in production mode
 //defined('YII_DEBUG') or define('YII_DEBUG',true);
@@ -10,12 +10,12 @@ $config=dirname(__FILE__).'/protected/config/main.php';
 //defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
 // include Composer's autoloader
-require_once(__DIR__.'/../vendor/autoload.php');
+require_once __DIR__ . '/../vendor/autoload.php';
 
 // increase memory limit and execution time
 ini_set('memory_limit', '1024M');
 ini_set('max_execution_time', 120);
 
-require_once($yii);
+require_once $yii;
 
 Yii::createWebApplication($config)->run();
--- src/protected/yiic.php
+++ src/protected/yiic.php
@@ -2,10 +2,10 @@
 
 // load environment from .env if it exists
 // include Composer's autoloader
-require_once __DIR__.'/../../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 // change the following paths if necessary
-$yiic=dirname(__FILE__).'/../../vendor/yiisoft/yii/framework/yiic.php';
-$config=dirname(__FILE__).'/config/console.php';
+$yiic   = __DIR__ . '/../../vendor/yiisoft/yii/framework/yiic.php';
+$config = __DIR__ . '/config/console.php';
 
 require_once($yiic);
