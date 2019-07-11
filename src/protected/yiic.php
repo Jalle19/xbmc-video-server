e687e33ac5bb406f7e20e58199c0ca96f50c3b64
Sam Stenvall <sam.stenvall@nordsoftware.com>
2019-07-11 19:48:19 +0300

    Load the Composer autoloader in yiic.php too

--- src/protected/yiic.php
+++ src/protected/yiic.php
@@ -1,5 +1,9 @@
 <?php
 
+// load environment from .env if it exists
+// include Composer's autoloader
+require_once __DIR__.'/../../vendor/autoload.php';
+
 // change the following paths if necessary
 $yiic=dirname(__FILE__).'/../../vendor/yiisoft/yii/framework/yiic.php';
 $config=dirname(__FILE__).'/config/console.php';
